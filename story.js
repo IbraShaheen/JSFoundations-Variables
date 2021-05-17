ab2faf5141c89fdf53788b1fb1cf3b23d92416b9
IbraShaheen <engibraheem1995@gmail.com>
2021-05-17 15:43:43 +0300

    vars

---
+++ story.js
@@ -0,0 +1,9 @@
+let name = "ibraheem";
+let age = 26;
+let hobby = "singing";
+
+//template literals
+console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
+
+//Concatenation
+console.log("Mr. "+ name + ", in 33 years, you will be "+ (age+33) + " years old. By then you'll have decades of "+ hobby + " practice!");
