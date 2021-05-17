ab5996d1a741d54b950bc54bc987258c6c1d3dc1
IbraShaheen <engibraheem1995@gmail.com>
2021-05-17 18:08:11 +0300

    test

--- story.js
+++ story.js
@@ -7,3 +7,5 @@ console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then 
 
 //Concatenation
 console.log("Mr. "+ name + ", in 33 years, you will be "+ (age+33) + " years old. By then you'll have decades of "+ hobby + " practice!");
+
+//finaltouch
