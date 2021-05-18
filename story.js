0307976588b881683d470bf89756c563cb23af2d
IbraShaheen <engibraheem1995@gmail.com>
2021-05-18 08:59:21 +0300

    test

--- story.js
+++ story.js
@@ -9,3 +9,4 @@ console.log(`Mr. ${name}, in 33 years, you will be ${age+33} years old. By then 
 console.log("Mr. "+ name + ", in 33 years, you will be "+ (age+33) + " years old. By then you'll have decades of "+ hobby + " practice!");
 
 //finaltouch
+// morning 18.05.2021
